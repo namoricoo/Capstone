24f94cc0b73a24b282ad578835ea134c9775d170
Nate <namoricoo@hotmail.com>
2013-12-25 10:07:32 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QuakiSmart Final Project
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capstone::Application.config.secret_token = 'b99dfd90f79f2cda04768421ef70fcded170c1164218e008381c35f3682192027f56f608a0fca129ad310597ee7c363eefc4518503205c50cf544e0d112fd942'
